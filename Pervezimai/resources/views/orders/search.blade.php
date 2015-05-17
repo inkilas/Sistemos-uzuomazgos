fd44d5562591e626dc632491484f51124eda4cd5
Kraniukas <e.r.n.e.s.t.a.s.1.6.u@gmail.com>
2015-05-17 23:20:20 +0300

    Sutrigerinta lentele su markeriais

--- Pervezimai/resources/views/orders/search.blade.php
+++ Pervezimai/resources/views/orders/search.blade.php
@@ -72,26 +72,13 @@
 @endsection
 @section('footer')
 <script type="text/javascript">
-    $('table td').on('click', function() {
-        if($(this).parent().hasClass('success')){
-            $(this).parent().removeClass('success');
-            $(this).parent().find('input').removeAttr('name');
-        }else{
-            $(this).parent().addClass('success');
-            $(this).parent().find('#auto_registration_id').attr('name', 'auto_registration_id[]');
-            $(this).parent().find('#provider_id').attr('name', 'provider_id[]');
-        }
-    })
-</script>
-<script type="text/javascript">
 
      var geocoder;
      var map;
      markers = [                          // sukuriam masyvą markerio koordinatėms. Pavad.,lat, lng, icon.
            ['Paėmimo adresas', , ,'/images/flagman.png'],
            ['Pristatymo adresas', , ,'/images/smiley_happy.png']
-            // ['Tarpinis adresas', , ,1]
-      ];
+    ];
      var markerBounds = new google.maps.LatLngBounds();
      var latlng;
      var directionsDisplay;
@@ -175,7 +162,7 @@
         var delay = 1000;
         setTimeout(showRoute, delay);
 
-     function showRoute(){
+     function showRoute(){                                  // paemimo ir pristatymo vietos
         location1 = markers[0][1] + "," + markers[0][2];
         location2 = markers[1][1] + "," + markers[1][2];
         directionsDisplay = new google.maps.DirectionsRenderer(
@@ -183,7 +170,7 @@
                 suppressMarkers: true,
                 suppressInfoWindows: true
             });
-        directionsDisplay.setMap(map);
+        directionsDisplay.setMap(map);                      // pradedam kursavima
             var request = {
                 origin:location1,
                 destination:location2,
@@ -193,25 +180,25 @@
             {
                 if (status == google.maps.DirectionsStatus.OK)
                     {
-                        var laikas = response.routes[0].legs[0].duration.text;
-                        var mapObj = {
+                        var laikas = response.routes[0].legs[0].duration.text;          // pasiimam laiko tekstine verte
+                        var mapObj = {                                                  // keisim angliskus zodzius i lietuviskus joje
                            day:"d.",
                            hour:"val.",
                            hours:"val.",
                            mins:"min."
                         };
-                        laikas = laikas.replace(/day|hours|hour|mins/gi, function(matched){
+                        laikas = laikas.replace(/day|hours|hour|mins/gi, function(matched){            // keiciam tekstines vertes
                           return mapObj[matched];
                         });
-                        directionsDisplay.setDirections(response);
+                        directionsDisplay.setDirections(response);                          // pasiimam laiko ir atstumo vertes
                         route_distance = response.routes[0].legs[0].distance.value;
                         route_duration = response.routes[0].legs[0].duration.value;
                         distance = "Atstumas tarp paėmimo ir pristatymo taškų yra: "+response.routes[0].legs[0].distance.text;
                         distance += ". Vidutinė kelionės trukmė: "+laikas;
-                        document.getElementById("distance_road").innerHTML = distance;
+                        document.getElementById("distance_road").innerHTML = distance;          // atvaizduojam distance_road id
                     }
             });
-        var line = new google.maps.Polyline({
+        var line = new google.maps.Polyline({               // nustatymai kelio linijos
             map: map,
             path: [location1, location2],
             strokeWeight: 7,
@@ -223,7 +210,7 @@
        var delay = 2500;
        setTimeout(showproviders, delay);
 
-       var auto_city = new Array();
+       var auto_city = new Array();                                 // issitraukiam duomenis is duomenu bazes i array
              @foreach($autos_by_categories as $auto_by_category)
                 @foreach($auto_by_category->user()->get() as $provider_by_category)
                     auto_city.push(['{{ $provider_by_category->id}}', '{{ $auto_by_category->id}}', '{{ $provider_by_category->name}}', '{{ $auto_by_category->auto_city}}', '{{ $auto_by_category->price_km}}', '{{ $auto_by_category->price_h}}', '{{ $auto_by_category->auto_comment = trim(preg_replace('/\s\s+/', ' ', $auto_by_category->auto_comment)) }}' ]);
@@ -231,27 +218,35 @@
              @endforeach
 
             
-             
+        var providers_markers = new Array();            // trigerinam lenteles click'a su markeriu
+        $('table tr').on('click', function() { 
+            for (i = 0; i < providers_markers.length; i++) {
+                if(providers_markers[i].title == $(this).attr('id')) {
+                    google.maps.event.trigger(providers_markers[i], 'click');
+                }
+            }    
+        });
 
     function showproviders() {
            
         $.each(auto_city, function(index, doc){
 
               $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+doc[3]+'&sensor=false', null, function (data) {
-                var p = data.results[0].geometry.location;
+                var p = data.results[0].geometry.location;              // is arejaus imam adresa ir isgeocodine dedam markerius
                 var latlng = new google.maps.LatLng(p.lat, p.lng);
                 var marker1 = new google.maps.Marker({
                     position: latlng,
                     icon: '/images/truck3.png',
                     map: map,
-                    title: doc[1]
+                    title: doc[1]                                   // vezejo vardas kaip markerio title
                 });
-                var provider_duration_price = (route_duration / 3600) * doc[5];
-                var provider_route_price = (route_distance / 1000) * doc[4];
-                var rounded_price_km = Math.round( provider_route_price * 10 ) / 10;
-                var rounded_price_h = Math.round( provider_duration_price * 10 ) / 10;
+                providers_markers.push(marker1);                    // i globalu kintamaji isstumiam
+                var provider_duration_price = (route_duration / 3600) * doc[5];             // apsk. kaina pagal laika
+                var provider_route_price = (route_distance / 1000) * doc[4];                // apsk. kaina pagal km
+                var rounded_price_km = Math.round( provider_route_price * 10 ) / 10;        // suapvalinam verte
+                var rounded_price_h = Math.round( provider_duration_price * 10 ) / 10;      // suapvalinam verte
 
-                var contentString = '<div id="content">'+
+                var contentString = '<div id="content">'+                                   // informacinio lango turinys
                 '<div id="bodyContent">'+
                 '<p align="justify"><b>Vežėjas:</b> '+doc[2]+'<br><b>Adresas: </b>'+doc[3]+'</b><br>'+
                 'Preliminari kelionės kaina pagal vežėjo įkainius už kilometrą yra:   <b><font color="red">'+rounded_price_km+' €;</font></b><br>'+
@@ -263,7 +258,7 @@
                       content: contentString
                   });
                  
-              google.maps.event.addListener(marker1, 'click', toggleBounce);
+              google.maps.event.addListener(marker1, 'click', toggleBounce);                // nustatom suoliavima paspaudus
               function toggleBounce() {
 
                   if (marker1.getAnimation() != null) {
@@ -273,7 +268,7 @@
                   }
               }
 
-              google.maps.event.addListener(marker1, 'click', function() {
+              google.maps.event.addListener(marker1, 'click', function() {                  // paspaudus ant markerio pazymim lenteles elementa
                    infowindow.open(map,marker1);
                    if($("#"+marker1.title).hasClass('success')){
                         $("#"+marker1.title).removeClass('success');
