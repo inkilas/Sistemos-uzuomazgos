b853e33d49555fedfd738a36fb4f68d996758761
Dainius <dainius.mieziunas@stud.vgtu.lt>
2015-05-18 15:58:50 +0300

    mini changes

--- Pervezimai/resources/views/orders/search.blade.php
+++ Pervezimai/resources/views/orders/search.blade.php
@@ -88,6 +88,19 @@
      var route_distance;
      var route_duration;
 
+    function detectBrowser() {
+      var useragent = navigator.userAgent;
+      var mapdiv = document.getElementById("map-canvas");
+
+      if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+        mapdiv.style.width = '100%';
+        mapdiv.style.height = '100%';
+      } else {
+        mapdiv.style.width = '600px';
+        mapdiv.style.height = '800px';
+      }
+    }
+
      google.maps.event.addDomListener(window, 'load', initialize);
 
      function initialize() {
